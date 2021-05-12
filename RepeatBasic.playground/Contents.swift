9600f8cfbaf4c2f244b76644d2d0940e00430f08
Kryukov Andrey <65622566+KryukovAndrey@users.noreply.github.com>
2021-05-12 21:54:22 +0300

    + Множества

--- RepeatBasic.playground/Contents.swift
+++ RepeatBasic.playground/Contents.swift
@@ -5,3 +5,15 @@ import UIKit
 var nameDict = ["Иван": 10, "Максим": 20]
 nameDict["Кирилл"] = 30
 print(nameDict)
+
+
+
+
+// Множества
+
+var setOne: Set = [1, 2, 3, 4]
+setOne.insert(5)
+setOne.insert(4)
+print(setOne)
+
+
